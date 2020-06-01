de1bd2308f6ebf715135f11a6600e605076c0a37
sandro gagoshidze <49387424+sannyybg@users.noreply.github.com>
2020-06-01 10:25:39 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vector1 = new Vectors(4, 6);
+            var vector2 = new Vectors(-1, 3);
+
+            var vector3 = vector1 + vector2;
+            var vector4 = vector1 - vector2;
+
+            Console.WriteLine("vectorebi tolia Tu ara : " + (vector1 == vector3));
+
+
+            // მიმართულების შეცვლა
+
+            var vector5 = --vector1;
+
+
+
+
+            // გასატესტად 
+            Console.WriteLine("X coord: " + vector5.ShowCoordsx() + " Y coord: " + vector5.ShowCoordsy());
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ vectorss.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    public class Vectors
+    { 
+    
+        public double Xcoordinate { get; set; }
+
+        public double Ycoordinate { get; set; }
+
+        public static Vectors operator + (Vectors vector1, Vectors vector2)
+        {
+            return new Vectors(vector1.Xcoordinate + vector2.Xcoordinate, vector1.Ycoordinate + vector2.Ycoordinate);
+            
+        }
+
+        public static Vectors operator - (Vectors vector1, Vectors vector2)
+        {
+            return new Vectors(vector1.Xcoordinate - vector2.Xcoordinate, vector1.Ycoordinate - vector2.Ycoordinate);
+
+        }
+
+        public static bool operator == (Vectors vector1, Vectors vector2)
+        {
+            return vector1.Xcoordinate == vector2.Xcoordinate && vector1.Ycoordinate == vector2.Ycoordinate;
+
+        }
+
+        public static bool operator != (Vectors vector1, Vectors vector2)
+        {
+            return vector1.Xcoordinate != vector2.Xcoordinate | vector1.Ycoordinate != vector2.Ycoordinate;
+
+        }
+
+        public static Vectors operator -- (Vectors vector1)
+        {
+            vector1.Xcoordinate = -vector1.Xcoordinate;
+            vector1.Ycoordinate = -vector1.Ycoordinate;
+            return vector1;
+
+        }
+
+        public static Vectors operator ++ (Vectors vector1)
+        {
+            return vector1;
+
+        }
+
+
+        public Vectors(double x, double y)
+        {
+            Xcoordinate = x;
+            Ycoordinate = y;
+        }
+
+        public double ShowCoordsx()
+        {
+            return Xcoordinate;
+        }
+
+        public double ShowCoordsy()
+        {
+            return Ycoordinate;
+        }
+    }
+}
